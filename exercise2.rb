cd7730a59d30b5e7568b78fcd1f0076e873c56f4
Vicky L <vicky.sy.loo@gmail.com>
2016-03-22 13:52:45 -0400

    exercise 2 completed

---
+++ exercise2.rb
@@ -0,0 +1,21 @@
+# Question 1
+
+puts "0.20 * 50"
+
+# 2. Question 2
+
+puts "This is a string" + " 5"
+
+#  Question 3
+
+puts "The results are #{45628 * 7839}"
+
+# Question 4
+
+# This equation will become true || true
+# The right side is true because the statements inside are true && true
+# The left side is true because it will come out to be !false, since 10 is not equal to 11
+# Therefore !false = true, making both sides of || true
+# Both sides are true which make the equation true
+
+(10 < 20 && 30 < 20) || !(10 == 11)
